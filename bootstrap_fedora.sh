5ee609e5463e0bfe377d0ab355becef652bdd019
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-03-09 17:46:19 +0100

    Add preupdating and cleaning

--- bootstrap_fedora.sh
+++ bootstrap_fedora.sh
@@ -15,6 +15,23 @@ mkdir -p "/tmp/$1"
 echo "Bootstrapping Fedora $1"
 dnf -y --releasever="$1" --installroot=/tmp/"$1" groupinstall core
 
+echo "Updating all packages inside rootfs"
+chroot /tmp/"$1" /bin/bash -c "dnf update -y"
+
+echo "Cleaning rootfs"
+# Remove unneeded/temporary files to reduce the rootfs size
+rm -rf /tmp/"$1"/boot/*
+rm -rf /tmp/"$1"/dev/*
+rm -rf /tmp/"$1"/lost+found/*
+rm -rf /tmp/"$1"/media/*
+rm -rf /tmp/"$1"/mnt/*
+rm -rf /tmp/"$1"/proc/*
+rm -rf /tmp/"$1"/run/*
+rm -rf /tmp/"$1"/sys/*
+rm -rf /tmp/"$1"/tmp/*
+rm -rf /tmp/"$1"/var/tmp
+rm -rf /tmp/"$1"/var/cache
+
 echo "Compressing rootfs"
 cd "/tmp/$1"
 tar -cv -I 'xz -9 -T0' -f ../fedora-rootfs-"$1".tar.xz ./
